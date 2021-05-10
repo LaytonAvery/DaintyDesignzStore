91889e2784bd464c79b53f535f53e2de487204c3
LaytonAvery <laytonavery5@gmail.com>
2021-05-10 00:06:35 -0400

    set up home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import HomeScreen from "./Screens/HomeScreen";
 
 const App = () => {
 	return (
 		<>
-			<h1>Welcome to Dainty Designz</h1>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<HomeScreen />
+				</Container>
+			</main>
+			<Footer />
 		</>
 	);
 };
